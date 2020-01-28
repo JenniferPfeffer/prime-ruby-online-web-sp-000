e03d221b4fa0f1800d98d3fea715d72c12b1000f
Jennifer Pfeffer <jennifer.pfeffer127@gmail.com>
2020-01-28 17:32:54 +0000

    Automatically backed up by Learn

--- prime.rb
+++ prime.rb
@@ -2,6 +2,7 @@
 def prime?(integer) 
  if integer < 0 || integer == 0 || integer == 1 
    return false 
- else (2..integer-1).to_a.all? do |factors| 
-   if integer % factors 
+ else (2..integer-1).to_a.all? do |numbers| 
+   if integer % numbers != 0 
+     
 end  
